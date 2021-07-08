4ad2253786ea282496ea83da99d93a28f8c7dd74
Pushkar Evgeni <evgeni.j6585@gmail.com>
2021-07-08 23:47:47 +1000

    Base-concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,25 @@
-function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+"use strict";
+  function solveEquation(a,b,c){
+    let D = b**2-4*a*c;
+    let x1;
+    let x2;
+    let x;
+    if(D > 0){
+      x1 =  (-b + Math.sqrt(D))/(2*a);
+      x2 =  (-b - Math.sqrt(D))/(2*a);
+      return "Первый корень: " + Math.round(x1)+ ", Второй корень: " + Math.round(x2);
+    }else if (D < 0){ return "Корней нет";
+    }else if (D === 0){ 
+      x = -b/(2*a);
+      return "Корень только один: x1 = " + Math.round(x);
+    }
+}
+  function calculateQuadraticEquation(){
+    let a = document.getElementById("a").value;
+    let b = document.getElementById("b").value;
+    let c = document.getElementById("c").value;
+    let result = solveEquation(a,b,c);
+    document.getElementById("result").textContent = result;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
