4be6a5e6031ad3260ad15ef34b032b81a8dacc84
Pushkar Evgeni <evgeni.j6585@gmail.com>
2021-07-18 14:57:05 +0300

    second commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -19,13 +19,7 @@
     let b = document.getElementById("b").value;
     let c = document.getElementById("c").value;
     let result = solveEquation(a,b,c);
-    document.getElementById("result").textContent = result;
+    document.getElementById("result").textContent = result; 
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
 
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
-}
